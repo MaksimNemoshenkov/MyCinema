75d4d56645ec199ff539787396d36c912dbcbca1
MaksimNemoshenkov <shalun084@gmail.com>
2020-11-30 21:23:00 +0200

    comit 4.6 I did something

--- src/main/java/com/Cinema/controllers/SeanceController.java
+++ src/main/java/com/Cinema/controllers/SeanceController.java
@@ -3,6 +3,7 @@ package com.Cinema.controllers;
 import com.Cinema.models.Hall;
 import com.Cinema.models.Movie;
 import com.Cinema.models.Seance;
+import com.Cinema.models.Ticket;
 import com.Cinema.repo.HallRepository;
 import com.Cinema.repo.MovieRepository;
 import com.Cinema.repo.SeanceRepository;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -38,13 +40,46 @@ public class SeanceController {
     }
 
     @PostMapping("/seance/add")
-    public String seancePostAdd (@RequestParam String data, @RequestParam int movie_id, @RequestParam int hall_id, Model model){
-        Seance seance = new Seance(data);
+    public String seancePostAdd (@RequestParam String date, @RequestParam int movie_id, @RequestParam int hall_id, Model model){
+        Seance seance = new Seance(date);
         Hall hall = hallRepository.findById(hall_id).orElseThrow();
         seance.setHall(hall);
         Movie movie = movieRepository.findById(movie_id).orElseThrow();
         seance.setMovie(movie);
         seanceRepository.save(seance);
         return "redirect:/seance";
+    }
+    @GetMapping("/seance/{id}")
+    public String seanceDetails(@PathVariable(value = "id") int id, Model model){
+        if(!seanceRepository.existsById(id)){
+            return "redirect:/seance";
+        }
+        model.addAttribute("seance", seanceRepository.findById(id).orElseThrow());
+        return "seance-detail";
+    }
+    @GetMapping("/seance/{id}/edit")
+    public String seanceEdit(@PathVariable(value = "id") int id, Model model){
+        if(!seanceRepository.existsById(id)){
+            return "redirect:/seance";
+        }
+        model.addAttribute("seance", seanceRepository.findById(id).orElseThrow());
+        return"seance-edit";
+    }
+    @PostMapping("/seance/{id}/edit")
+    public String cinemaSeanceUpdate(@PathVariable(value = "id") int id, @RequestParam String date, @RequestParam int movie_id, @RequestParam int hall_id, Model model){
+        Seance seance = seanceRepository.findById(id).orElseThrow();
+        seance.setDate(date);
+        Hall hall = hallRepository.findById(hall_id).orElseThrow();
+        seance.setHall(hall);
+        Movie movie = movieRepository.findById(movie_id).orElseThrow();
+        seance.setMovie(movie);
+        seanceRepository.save(seance);
+        return"redirect:/seance";
+    }
+
+    @PostMapping("/seance/{id}/remove")
+    public String cinemaSeanceDelete(@PathVariable(value = "id") int id, Model model){
+        seanceRepository.delete(seanceRepository.findById(id).orElseThrow());
+        return"redirect:/seance";
     }
 }
